edcbe09f81241791f094963e4d29432bb4f77c81
venakatasatya.tutika <anilkumartutika000@gmail.com>
2019-08-03 17:08:54 +0530

    Coolections and Generics Implemented

---
+++ src/main/java/com/stackroute/collectionsandgenerics/CollectionsImplementation.java
@@ -0,0 +1,28 @@
+package com.stackroute.collectionsandgenerics;
+
+import java.util.Hashtable;
+import java.util.Vector;
+
+public class CollectionsImplementation {
+    public static void main(String[] args) {
+        // Creating instances of array, vector and hashtable
+        Vector<Integer> v = new Vector();
+        Hashtable<Integer, String> h = new Hashtable();
+        v.addElement(1);
+        v.addElement(2);
+        h.put(1, "Anil");
+        h.put(2, "Aarron");
+
+        // Array instance creation requires [], while Vector
+        // and hastable require ()
+        // Vector element insertion requires addElement(), but
+        // hashtable element insertion requires put()
+
+        // Accessing first element of array, vector and hashtable
+        System.out.println(v.elementAt(0));
+        System.out.println(h.get(1));
+
+        // Array elements are accessed using [], vector elements
+        // using elementAt() and hashtable elements using get()
+    }
+}
